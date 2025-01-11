b1fbc248158f85464b8ff094949e90c00733e34f
Mtende Kuyokwa <mtendekuyokwa19@gmail.com>
2025-01-11 09:14:17 +0200

    creating and styling the navbar

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -1,10 +1,88 @@
+import { useState } from "react"
+import { IoLogoGithub } from "react-icons/io"
+import { IoMoon, IoSunny } from "react-icons/io5"
 import { Outlet } from "react-router"
 
 export const Navbar = () => {
   return (
-    <div className="text-3xl font-bold underline">
-      navbar
+    <div className="bg-slate-200  dark:text-white dark:bg-everNav dark:text-everNavText      h-16 flex w-screen items-center">
+      <HeaderMenu />
       <Outlet />
     </div>
   )
 }
+function HeaderMenu() {
+  return (<div className="flex w-full justify-evenly">
+    <Avatar />
+
+    <div className="flex font-Quicksand font-bold gap-8 justify-center items-center">
+      <p>Blog</p>
+      <p>About</p>
+      <p>Lists</p>
+    </div>
+
+    <div className="flex gap-4 justify-center items-center">
+      <ToogleDark iconSize={25} />
+
+      <Githubbtn />
+    </div>
+
+  </div>)
+}
+
+
+function Githubbtn() {
+  return (
+    <button>
+
+      <a href=""><IoLogoGithub size={25} /></a>
+    </button>
+  )
+
+}
+function imgUrl(url: string) {
+  return new URL(url)
+
+}
+function Avatar() {
+  return (
+    <div className="flex justify-center items-center gap-2">
+      <div className="rounded-full w-12 flex justify-center items-center h-12 bg-red-600">
+        <p className="text-white">
+          <img src={imgUrl("./assets/header.jpg")} alt="mypfp" />
+          MK
+        </p>
+
+      </div>
+      <div className="flex  font-JetBrains justify-center text-emerald-700 dark:text-headerGreen items-center text-xl font-bold ">
+
+
+        <p>~</p>
+
+        <p>/</p>
+        <p>mtende</p>
+        <p>/</p>
+      </div>
+    </div>
+  )
+
+}
+
+function ToogleDark({ iconSize }: Itoogledark) {
+  const [dark, setDark] = useState(false)
+  function darkModeHandler() {
+    setDark(!dark)
+    document.body.classList.toggle("dark")
+  }
+
+  return (
+
+    <button onClick={darkModeHandler}>
+      {dark ? <IoSunny size={iconSize} /> : <IoMoon size={iconSize} />}
+    </button>
+  )
+
+}
+interface Itoogledark {
+  iconSize: number
+} 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors")
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+
+      fontFamily: {
+        JetBrains: ['JetBrains Mono', 'sans-serif'],
+        Quicksand: ['Quicksand', 'sans-serif']
+      }
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+
+      'headerGreen': '#a3bd8d',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'everNav': '#232a2d',
+
+      'everNavText': '#b3b9b8',
+      ...colors
+    },
   },
   plugins: [],
 }
